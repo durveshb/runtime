098236ca1cea6533244d9358ba44a9186f450b40
Durvesh Bhalekar <durvesh.5.db@gmail.com>
2023-11-24 16:37:27 +0530

    add geo tracker

--- app/run/_components/RunTimer.tsx
+++ app/run/_components/RunTimer.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { use, useCallback } from "react";
 import { useSession } from "../_hooks/useSession";
 import Progress from "./Progress";
 import Controls from "./Controls";
 import { Runtime } from "@/lib/types";
+import { useTrackRuntimeLocation } from "../_hooks/useTrackRuntimeLocation";
+import { Divide } from "lucide-react";
 
 const RunTimer = ({ runtime }: { runtime: Runtime }) => {
   const {
@@ -10,13 +12,25 @@ const RunTimer = ({ runtime }: { runtime: Runtime }) => {
     totalPhases,
     timeElapsed,
     phaseTarget,
-    play,
-    pause,
+    play: playSession,
+    pause: pauseSession,
     isPaused,
     isComplete,
   } = useSession({
     session: runtime,
   });
+  const { coords, startTracking, stopTracking } = useTrackRuntimeLocation();
+
+  const play = useCallback(() => {
+    playSession();
+    startTracking();
+  }, [playSession, startTracking]);
+
+  const pause = useCallback(() => {
+    pauseSession();
+    stopTracking();
+  }, [pauseSession, stopTracking]);
+
   return (
     <div className="flex flex-col gap-5 items-center justify-center">
       <Progress
---
+++ app/run/_hooks/useTrackRuntimeLocation.ts
@@ -0,0 +1,52 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export const useTrackRuntimeLocation = () => {
+  const [isTracking, setIsTracking] = useState<boolean>(false);
+  const [coords, setCoords] = useState<
+    {
+      latitude: number;
+      longitude: number;
+    }[]
+  >([]);
+  const watcherRef = useRef<number>();
+
+  useEffect(() => {
+    if (isTracking) {
+      console.log("Started traking user location");
+      watcherRef.current = navigator.geolocation.watchPosition(
+        ({ coords }) => {
+          setCoords((prev) => [
+            ...prev,
+            {
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+            },
+          ]);
+        },
+        ({ message }) => {
+          console.log(message);
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 5000,
+        }
+      );
+    }
+
+    return () => {
+      if (watcherRef.current) {
+        navigator.geolocation.clearWatch(watcherRef.current);
+        console.log("Stopped traking user location");
+      }
+    };
+  }, [isTracking]);
+
+  const startTracking = useCallback(() => {
+    setIsTracking(true);
+  }, []);
+  const stopTracking = useCallback(() => {
+    setIsTracking(false);
+  }, []);
+
+  return { coords, startTracking, stopTracking };
+};
